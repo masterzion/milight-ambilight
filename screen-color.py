5e02f5281337a3a891eca178cc68b7cae7f23885
Jairo Moreno <master.zion@gmail.com>
2015-04-08 15:18:18 -0300

    cleaning the code

--- screen-color.py
+++ screen-color.py
@@ -65,13 +65,13 @@ config.read( app_path + "/config.ini")
 milight_hostname = config.get('MILIGHT','hostname')
 milight_port     = config.getint('MILIGHT','port')
 pixel_interval   = config.getint('CPU_OPTMIZATION', 'pixel_interval') # interval between pixels
-time_interval    =  config.getfloat('CPU_OPTMIZATION', 'time_interval') # time interval 
-debug            =  config.getboolean('CPU_OPTMIZATION', 'debug') # show color
+time_interval    = config.getfloat('CPU_OPTMIZATION', 'time_interval') # time interval 
+debug            = config.getboolean('CPU_OPTMIZATION', 'debug') # show color
 
 points = MonitoredPoints(pixel_interval)
 count = len(points)
 
-controller = milight.MiLight({'host': milight_hostname, 'port': 8899}, wait_duration=0) 
+controller = milight.MiLight({'host': milight_hostname, 'port': milight_port}, wait_duration=0) 
 
 # main loop
 while True:
