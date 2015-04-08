d79bea84f8127a4cfc4cb633534b629e06df150b
Jairo Moreno <master.zion@gmail.com>
2015-04-08 12:16:17 -0300

    Added python-milight

--- screen-color.py
+++ screen-color.py
@@ -5,12 +5,13 @@
 #
 # Get One single color to represent the screen
 # 
-# TODO: Apply to python-milight
+
 
 import gtk.gdk
 import sys
 import wx
 from time import sleep
+import milight
 
 # interval between pixels
 # less interval will increase the cpu usage
@@ -18,7 +19,7 @@ interval = 100
 
 
 # Create an array of points (pixels) to be monitored
-def MonitoredPoints():
+def MonitoredPoints(interval):
 	app = wx.PySimpleApp()
 	screensize = wx.GetDisplaySize()
 
@@ -39,13 +40,14 @@ def MonitoredPoints():
 
 def CurrentColor(points, pointcount):
     # get screen point colors
+    # http://stackoverflow.com/questions/27395968/get-screen-pixel-color-linux-python3
 	w = gtk.gdk.get_default_root_window()
 	sz = w.get_size()
 	pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
 	pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
 	pixel_array = pb.get_pixels_array()
 
-    #sum colors of all monitored pixels
+	#sum colors of all monitored pixels
 	red = 0
 	green = 0
 	blue = 0
@@ -63,12 +65,23 @@ def CurrentColor(points, pointcount):
 
 	return (red, green, blue)
 
-points = MonitoredPoints()
-pointcount = len(points)
 
+# http://stackoverflow.com/questions/214359/converting-hex-color-to-rgb-and-vice-versa
+#def hex_to_rgb(value):
+#	value = value.lstrip('#')
+#	lv = len(value)
+#	return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+#def rgb_to_hex(rgb):
+#	return '#%02x%02x%02x' % rgb
+
+points = MonitoredPoints(interval)
+pointcount = len(points)
+controller = milight.MiLight({'host': '192.168.1.110', 'port': 8899}, wait_duration=0) 
 
 # execute each 0.1 milisecond
 while True:
 	actualcolor =CurrentColor(points, pointcount)
-	print actualcolor
+	milight.color_from_rgb(actualcolor[0], actualcolor[1], actualcolor[2])
+#	print  rgb_to_hex( actualcolor )
 	sleep(0.1)
