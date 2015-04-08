2aab91a505db5e2470a2984ea71d70bab0e711d9
Jairo Moreno <master.zion@gmail.com>
2015-04-08 13:50:50 -0300

    cleaning the code

--- screen-color.py
+++ screen-color.py
@@ -16,62 +16,48 @@ from time import sleep
 
 # Create an array of points (pixels) to be monitored
 def MonitoredPoints(interval):
-	app = wx.PySimpleApp()
-	screensize = wx.GetDisplaySize()
+    app = wx.PySimpleApp()
+    screensize = wx.GetDisplaySize()
 
-	countx = screensize[0]/interval
-	county = screensize[1]/interval
-	actualcolor = (0,0,0)
-	totalpoints = countx * county
-	monitoredpoints = []
+    countx = screensize[0]/interval
+    county = screensize[1]/interval
 
-	for x in range(0, countx):
-		for y in range(0, county):
-			monitoredpoints.append([x * interval, y * interval])
+    monitoredpoints = []
+    for x in range(0, countx):
+        for y in range(0, county):
+            monitoredpoints.append([x * interval, y * interval])
 
-	return monitoredpoints
+    return monitoredpoints
 
 def CurrentColor(points, count):
-	# get screen point colors
-	# http://stackoverflow.com/questions/27395968/get-screen-pixel-color-linux-python3
-	w = gtk.gdk.get_default_root_window()
-	sz = w.get_size()
-	pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
-	pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
-	pixel_array = pb.get_pixels_array()
+    # get screen point colors based in
+    # http://stackoverflow.com/questions/27395968/get-screen-pixel-color-linux-python3
+    w = gtk.gdk.get_default_root_window()
+    sz = w.get_size()
+    pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
+    pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
+    pixel_array = pb.get_pixels_array()
 
-	#sum colors of all monitored pixels
-	red = 0
-	green = 0
-	blue = 0
-	for point in points:
-		color = pixel_array[point[1]] [point[0]]
+    #sum colors of all monitored pixels
+    red = 0
+    green = 0
+    blue = 0
+    for point in points:
+        color = pixel_array[point[1]] [point[0]]
 
-		red   = red + color[0]
-		green = green + color[1]
-		blue  = blue + color[2]
+        red   = red + color[0]
+        green = green + color[1]
+        blue  = blue + color[2]
 
-	# divide by point count
-	red = red / count
-	green = green / count
-	blue = blue / count
+    # divide by point count
+    red = red / count
+    green = green / count
+    blue = blue / count
 
-	return (red, green, blue)
-
-
-# http://stackoverflow.com/questions/214359/converting-hex-color-to-rgb-and-vice-versa
-#def hex_to_rgb(value):
-#	value = value.lstrip('#')
-#	lv = len(value)
-#	return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
-
-#def rgb_to_hex(rgb):
-#	return '#%02x%02x%02x' % rgb
-
+    return (red, green, blue)
 
 # load config files
 app_path = os.path.dirname(os.path.realpath(__file__))
-
 
 config = SafeConfigParser()
 config.read( app_path + "/config.ini")
@@ -89,9 +75,9 @@ controller = milight.MiLight({'host': milight_hostname, 'port': 8899}, wait_dura
 
 # main loop
 while True:
-	actualcolor =CurrentColor(points, count)
-	milight.color_from_rgb(actualcolor[0], actualcolor[1], actualcolor[2])
-	if debug :
-		print   actualcolor 
-	sleep(time_interval)
+    actualcolor =CurrentColor(points, count)
+    milight.color_from_rgb(actualcolor[0], actualcolor[1], actualcolor[2])
+    if debug :
+        print   actualcolor 
+    sleep(time_interval)
 
